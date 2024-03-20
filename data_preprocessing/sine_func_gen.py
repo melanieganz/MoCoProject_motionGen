e0907fcaa72b09fa6dfc6a129a63a63cd665c5b2
H. A. Georgiev <151514524+hr-ge@users.noreply.github.com>
2024-03-20 12:48:48 +0100

    Added another method for correlating sine waves.

--- data_preprocessing/sine_func_gen.py
+++ data_preprocessing/sine_func_gen.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Function to generate multiple sine waves as one sample
 def generate_uncorrelated_sine_waves(num_samples, num_waves, frequency_range, amplitude_range):
     waves = np.zeros((num_samples, num_waves))
 
     for i in range(num_waves):
         frequency = np.random.uniform(*frequency_range)
         amplitude = np.random.uniform(*amplitude_range)
-        phase = np.random.uniform(-np.pi, np.pi)
+        phase     = np.random.uniform(-np.pi, np.pi)
 
         x = np.linspace(0, 2*np.pi, num_samples)
         wave = amplitude * np.sin(frequency * x + phase)
@@ -16,21 +15,49 @@ def generate_uncorrelated_sine_waves(num_samples, num_waves, frequency_range, am
 
     return waves
 
-def generate_correlated_sine_waves(num_samples, num_waves, frequency_range, amplitude_range):
+
+
+def generate_correlated_sine_waves_one(num_samples, num_waves, frequency_range, amplitude_range):
+    waves = np.zeros((num_samples, num_waves))
+
+    # Generate a random scaler:
+    s = np.random.uniform(0, 1)
+
+    frequency = np.random.uniform(*frequency_range)
+    phase     = np.random.uniform(-np.pi, np.pi)
+
+    x = np.linspace(0, 2*np.pi, num_samples)
+
+    # Generate three sine waves and correlate them using a fourth one:
+    for i in range(num_waves):
+        amplitude = np.random.uniform(*amplitude_range)
+
+        wave = amplitude * np.sin(frequency * x + phase)
+
+        # Multiply the generated sine waves by the fourth one:
+        correlated_wave = s * wave
+        waves[:, i] = correlated_wave
+
+    return waves
+
+
+
+def generate_correlated_sine_waves_two(num_samples, num_waves, frequency_range, amplitude_range):
     waves = np.zeros((num_samples, num_waves))
 
     # Generate the fourth sine wave:
-    frequency = np.random.uniform(*frequency_range)
+    frequency   = np.random.uniform(*frequency_range)
     amplitude_s = np.random.uniform(*amplitude_range)
-    phase_s = np.random.uniform(-np.pi, np.pi)
+    phase_s     = np.random.uniform(-np.pi, np.pi)
 
     x = np.linspace(0, 2*np.pi, num_samples)
+
     wave_4th = amplitude_s * np.sin(frequency * x + phase_s)
 
     # Generate three sine waves and correlate them using a fourth one:
     for i in range(num_waves):
         amplitude = np.random.uniform(*amplitude_range)
-        phase = np.random.uniform(-np.pi, np.pi)
+        phase     = np.random.uniform(-np.pi, np.pi)
 
         wave = amplitude * np.sin(frequency * x + phase)
 
@@ -40,8 +67,10 @@ def generate_correlated_sine_waves(num_samples, num_waves, frequency_range, ampl
 
     return waves
 
+
+
 # Number of sample points to generate
-sample_points = 200
+sample_points = 100
 # Number of waves per sample
 num_waves = 3
 # Frequency range for generated sine waves
@@ -56,14 +85,15 @@ sine_wave_samples = np.zeros((num_samples_to_generate, sample_points, num_waves)
 
 # Generate sine wave samples
 for i in range(num_samples_to_generate):
-    sine_wave_samples[i] = generate_correlated_sine_waves(sample_points, num_waves, frequency_range, amplitude_range)
+    sine_wave_samples[i] = generate_correlated_sine_waves_one(sample_points, num_waves, frequency_range, amplitude_range)
 
-np.savez('custom_sines.npz', data=sine_wave_samples)
+print(sine_wave_samples.shape)
+np.savez('/path/to/custom_sines.npz', data=sine_wave_samples)
 
 # Plot the first sample from the array
 plt.figure(figsize=(12, 8))
 for i in range(num_waves):
-    plt.plot(sine_wave_samples[100, :, i], label=f'Sine Wave {i+1}')
+    plt.plot(sine_wave_samples[0, :, i], label=f'Sine Wave {i+1}')
 plt.plot()
 
 plt.title('Generated Sine Wave Sample')
