c08cbbde72cd986b1b7942789477964f09b8591b
Vincent Beliveau <vincent.beliveau@nru.dk>
2024-03-31 16:33:49 +0200

    updated Jakob's experiment

--- experiment/generate_sines/generate_sines.py
+++ experiment/generate_sines/generate_sines.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--no', type=int)
+parser.add_argument('--n_samples', type=int)
 parser.add_argument('--seq_len', type=int)
 parser.add_argument('--dim', type=int)
 parser.add_argument('--freq_range', nargs='+', type=float)
@@ -14,10 +14,57 @@ parser.add_argument('--phase_range', nargs='+', type=float)
 args = parser.parse_args()
 
 # run experiment
-titles = ['jakob_gen', 'timegan_gen_1', 'timegan_gen_2', 'timegan_gen_3', 'timegan_gen_4']
-generators = [utils.jakob_gen, utils.timegan_gen_1, utils.timegan_gen_2, utils.timegan_gen_3, utils.timegan_gen_4]
+titles = ['jakob_gen', 'timegan_gen', 'timegan_gen_1', 'timegan_gen_2']
+
 for gen, title in zip(generators, titles):
-    X = gen(args.no, args.seq_len, args.dim, args.freq_range, args.amp_range, args.phase_range)
+
+	# Old generator from Jakob
+	if titles == "jakob_gen":
+		utils.jakob_gen(
+			n_samples=args.n_samples,
+			freq_range=args.seq_len,
+			dim=args.dim,
+			freq_range=args.freq_range,
+			amp_range=args.amp_range,
+			phase_range=args.phase_range
+		)
+
+	# Original
+	if titles == "utils.timegan_gen":
+		utils.jakob_gen(
+			n_samples=args.n_samples,
+			x=None,
+			freq_range=args.seq_len,
+			dim=args.dim,
+			freq_range=args.freq_range,
+			amp_range=None,
+			phase_range=args.phase_range
+		)
+
+	x = np.linspace(0, 2*np.pi, args.seq_len)
+
+	if titles == "utils.timegan_gen_1":
+		utils.jakob_gen(
+			n_samples=args.n_samples,
+			x=x,
+			freq_range=args.seq_len,
+			dim=args.dim,
+			freq_range=args.freq_range,
+			amp_range=None,
+			phase_range=args.phase_range
+		)
+
+	if titles == "utils.timegan_gen_2":
+		utils.jakob_gen(
+			n_samples=args.n_samples,
+			x=x,
+			freq_range=args.seq_len,
+			dim=args.dim,
+			freq_range=args.freq_range,
+			amp_range=args.amp_range,
+			phase_range=args.phase_range
+		)
+
     X_train, X_test = train_test_split(X, train_size=0.8, random_state=1)
 
     # save data to timeVAE and TSGBench
--- experiment/generate_sines/utils.py
+++ experiment/generate_sines/utils.py
@@ -5,94 +5,74 @@ import matplotlib.pyplot as plt
 np.random.seed(1)
 
 # this is our old generator
-def jakob_gen(no=10000, seq_len=24, dim=6, freq_range=(1.0, 5.0), amp_range=(0.1, 0.9), phase_range=(-np.pi, np.pi)):
+def jakob_gen(
+        n_samples=10000,
+        seq_len=24,
+        dim=6,
+        freq_range=(1.0, 5.0),
+        amp_range=(0.1, 0.9),
+        phase_range=(-np.pi, np.pi)
+    ):
+
     data = list()
     x = np.linspace(0, 2*np.pi, seq_len)
-    for _ in range(no):
+    for n_sample in range(n_samples):
         waves = np.zeros((seq_len, dim))
+        rng = np.random.default_rng(seed=n_sample)
         for i in range(dim):
             # all waves have random frequency, amplitude and phase
-            frequency = np.random.uniform(*freq_range)
-            amplitude = np.random.uniform(*amp_range)
-            phase = np.random.uniform(*phase_range)
+            frequency = rng.uniform(*freq_range)
+            phase = rng.uniform(*phase_range)
+            amplitude = rng.uniform(*amp_range)
             wave = amplitude * np.sin(frequency * x + phase) 
             waves[:, i] = wave
         data.append(waves)
     return data
-    
-# this is the TimeGAN generator (no modifications)
-def timegan_gen_1(no=10000, seq_len=24, dim=6, freq_range=(1.0, 5.0), amp_range=(0.1, 0.9), phase_range=(-np.pi, np.pi)):
+
+
+def timegan_gen(
+        n_samples=10000,
+        x=None,
+        seq_len=24,
+        dim=6, freq_range=(1.0, 5.0),
+        amp_range=None,
+        phase_range=(-np.pi, np.pi)
+    ):
+
     data = list()
-    for _ in range(no):
+
+    for n_sample in range(n_samples):
+        
         temp = list()
-        for _ in range(dim):
-            # all waves have random frequency and phase
-            freq = np.random.uniform(*freq_range)
-            phase = np.random.uniform(*phase_range)
-            temp_data = [np.sin(freq * j + phase) for j in range(seq_len)]
+        rng = np.random.default_rng(seed=n_sample)
+
+        if x is None:
+            x = range(seq_len)
+
+        for n_sine in range(dim):
+
+            freq = rng.uniform(freq_range)
+            phase = rng.uniform(*phase_range)
+            if amp_range is None:
+                amp = 1.
+            else:
+                amp = np.random.uniform(*amp_range)
+
+            temp_data = [amp * np.sin(freq * x_ + phase) for x_ in x]
             temp.append(temp_data)
+
         temp = np.transpose(np.asarray(temp))
         temp = (temp + 1)*0.5
         data.append(temp)
     return data    
 
-# this is the TimeGAN generator (modified x-axis)
-def timegan_gen_2(no=10000, seq_len=24, dim=6, freq_range=(1.0, 5.0), amp_range=(0.1, 0.9), phase_range=(-np.pi, np.pi)):
-    data = list()
-    x = np.linspace(0, 2*np.pi, seq_len)
-    for _ in range(no):
-        temp = list()
-        for _ in range(dim):
-            # all waves have random frequency and phase
-            freq = np.random.uniform(*freq_range)
-            phase = np.random.uniform(*phase_range)
-            temp_data = np.sin(freq * x + phase)
-            temp.append(temp_data)
-        temp = np.transpose(np.asarray(temp))
-        temp = (temp + 1)*0.5
-        data.append(temp)
-    return data
-
-# this is the TimeGAN generator (modified x-axis, random amplitude per wave)
-def timegan_gen_3(no=10000, seq_len=24, dim=6, freq_range=(1.0, 5.0), amp_range=(0.1, 0.9), phase_range=(-np.pi, np.pi)):
-    data = list()
-    x = np.linspace(0, 2*np.pi, seq_len)
-    for _ in range(no):
-        temp = list()
-        for _ in range(dim):
-            # all waves have random frequency, amplitude and phase
-            freq = np.random.uniform(*freq_range)
-            amp = np.random.uniform(*amp_range)
-            phase = np.random.uniform(*phase_range)
-            temp_data = amp * np.sin(freq * x + phase)
-            temp.append(temp_data)
-        temp = np.transpose(np.asarray(temp))
-        data.append(temp)
-    return data
-
-# this is the TimeGAN generator (modified x-axis, random amplitude across samples, similar within each sample)
-def timegan_gen_4(no=10000, seq_len=24, dim=6, freq_range=(1.0, 5.0), amp_range=(0.1, 0.9), phase_range=(-np.pi, np.pi)):
-    data = list()
-    x = np.linspace(0, 2*np.pi, seq_len)
-    for _ in range(no):
-        temp = list()
-        for _ in range(dim):
-            # all waves have random frequency and phase
-            freq = np.random.uniform(*freq_range)
-            phase = np.random.uniform(*phase_range)
-            temp_data = np.sin(freq * x + phase)
-            temp.append(temp_data)
-        temp = np.transpose(np.asarray(temp))
-        amp = np.random.uniform(*amp_range)
-        temp = temp * amp
-        data.append(temp)
-    return data
 
 # plots a random sample
 def plot_sines(data, title):
     _ = plt.figure()
     n, (_, d) = len(data), data[-1].shape
-    idx = np.random.randint(0, n)
+    rng = np.random.default_rng(seed=42)
+    idx = rng.randint(0, n)
     for i in range(d):
         plt.plot(data[idx][:, i])
     plt.title(title)
